f2ee0430a9cf0fea001edc6aa57e038312870315
Memby Poudel <officialmembypoudel@gmail.com>
2023-12-05 20:48:37 +0545

    First Commit

---
+++ Excercise1/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Excercise1
+{
+	public class Car
+	{
+		private string brand;
+		private int year;
+		private float price;
+		public Car(string brand, int year, float price)
+		{
+			this.brand = brand;
+			this.year = year;
+			this.price = price;
+
+		}
+
+		//creating a method to print cardetails as the variables are private
+
+		public void PrintCarDetails()
+		{
+			Console.WriteLine($"The brand of car is {brand} made in {2023} has a price of ${price}.");
+		}
+
+	}
+}
+
---
+++ Excercise1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Excercise1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Running, Program!");
+
+        Car car = new Car("Toyota", 2023, 35000.99F);
+        car.PrintCarDetails();
+
+        Console.ReadKey();
+    }
+}
+
